e1fa3f60cf796a12a817b4d956c6eff0e56b37a5
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-06-25 19:56:49 -0300

    feature: ad faq page

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 export default function () {
     return (
         <main className="flex h-16 w-full flex-col">
             <div className="flex flex-grow items-center justify-center border-b-2 border-solid border-gray-200 font-sans font-medium">
                 <div className="flex w-3/4">
                     <div className="flex w-full justify-between text-xl">
-                        <p>eternizing</p>
+                        <Link href="/">eternizing</Link>
                         <div className="flex">
-                            <p>FAQ</p>
+                            <Link href="/faq">FAQ</Link>
                         </div>
                         <p>Sign Out</p>
                     </div>
---
+++ app/faq/page.tsx
@@ -0,0 +1,18 @@
+import Header from '../components/Header/Header'
+import Sidebar from '../components/Sidebar/Sidebar'
+
+export default function faq() {
+    return (
+        <main className="flex min-h-screen flex-col">
+            <Header />
+            <div className="flex min-h-full w-full flex-1 flex-col justify-between font-mono text-sm">
+                <div className="flex h-full flex-1">
+                    <Sidebar />
+                    <div className="flex">
+                        <p>faq</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}

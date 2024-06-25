e981ce974c8a688fbc2a1eef291c65f44b94590f
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-06-25 19:17:40 -0300

    feature: add prettierc config and initial code

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+export default function () {
+    return (
+        <main className="flex h-16 w-full flex-col">
+            <div className="flex flex-grow items-center justify-center border-b-2 border-solid border-gray-200 font-sans">
+                <div className="flex w-3/4">
+                    <div className="flex w-full justify-between text-xl">
+                        <p>eternizing</p>
+                        <div className="flex">
+                            <p>FAQ</p>
+                        </div>
+                        <p>Sign Out</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}

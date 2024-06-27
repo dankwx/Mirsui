8a582f6b9c74b507da756ac70ac5d8c65489a169
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-06-27 19:55:19 -0300

    add video url ranking

--- app/addvideo/page.tsx
+++ app/addvideo/page.tsx
@@ -1,53 +1,97 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import {
-    getFirestore,
-    collection,
-    doc,
-    setDoc,
-    getDoc,
-    getDocs,
-    query,
-} from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import firebaseConfig from '../firebase-config'
 import { initializeApp } from 'firebase/app'
 import Header from '../../components/Header/Header'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
 const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
 const auth = getAuth(app)
 
 export default function AddVideo() {
-    const [inputValue, setInputValue] = useState('')
-    const [subcollectionItemCount, setSubcollectionItemCount] = useState<
-        number | null
-    >(null)
+    const [loggedUser, setLoggedUser] = useState<unknown | null>(null)
+    const [authStateChangedComplete, setAuthStateChangedComplete] =
+        useState(false)
+    const [videoUrl, setvideoUrl] = useState('')
 
-    // Função para lidar com a consulta da subcoleção
-    const handleCheckSubcollection = async () => {
-        try {
-            // Monta o caminho para a subcoleção com base no input
-            const subcollectionPath = `claimed-videos/${inputValue}/subcolecao1`
+    const supabase = createClientComponentClient()
 
-            // Cria uma referência para a subcoleção
-            const subcollectionRef = collection(db, subcollectionPath)
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            if (user) {
+                setLoggedUser(user.uid)
+                console.log('logado')
+            } else {
+                setLoggedUser(null)
+                console.log('não logado')
+            }
+            setAuthStateChangedComplete(true) // Marca como completo após a execução
+        })
 
-            // Realiza a consulta para obter todos os documentos na subcoleção
-            const querySnapshot = await getDocs(subcollectionRef)
+        return () => unsubscribe()
+    }, [auth])
 
-            // Define o estado com o número de documentos encontrados
-            setSubcollectionItemCount(querySnapshot.size)
-
-            // Exibe um alerta com a quantidade de documentos
-            alert(
-                `Quantidade de itens na subcoleção ${inputValue}: ${querySnapshot.size}`
-            )
-        } catch (error) {
-            console.error('Erro ao consultar a subcoleção:', error)
+    // add video to supabase
+    const handleClaimVideo = async () => {
+        if (!loggedUser) {
+            console.log('Usuário não autenticado.')
+            return
         }
+
+        try {
+            if (!loggedUser) {
+                throw new Error('UID do usuário não encontrado.')
+            }
+
+            const { data: users, error: usersError } = await supabase
+                .from('users')
+                .select('id')
+                .eq('uid', loggedUser)
+                .single()
+
+            if (usersError) {
+                throw usersError
+            }
+
+            const user_id = users ? users.id : null
+
+            // Consulta para contar quantas vezes a video_url já foi inserida
+            const { count: videoCount, error: countError } = await supabase
+                .from('videos')
+                .select('*', { count: 'exact' })
+                .eq('video_url', videoUrl)
+
+            if (countError) {
+                throw countError
+            }
+
+            // A próxima posição será a contagem atual + 1
+            const nextPosition = videoCount !== null ? videoCount + 1 : 1
+
+            // Insere o vídeo na tabela 'videos'
+            const { data, error } = await supabase.from('videos').insert({
+                video_url: videoUrl,
+                user_id: user_id,
+                position: nextPosition,
+            })
+
+            if (error) {
+                throw error
+            }
+
+            // Limpa o campo de input após a inserção bem-sucedida
+            setvideoUrl('')
+        } catch (error) {
+            console.error('Erro ao reinvidicar vídeo:', error)
+        }
+    }
+
+    if (!authStateChangedComplete) {
+        return null // Ou algum indicador de carregamento, se necessário
     }
 
     return (
@@ -58,20 +102,20 @@ export default function AddVideo() {
                     <Sidebar />
                     <div className="flex flex-col p-4">
                         <p className="mb-4 text-lg font-semibold">
-                            Verificar Quantia
+                            Reinvidicar Video
                         </p>
                         <div className="flex flex-col space-y-2">
                             <input
                                 type="text"
                                 className="rounded-md border border-gray-300 p-2"
-                                value={inputValue}
-                                onChange={(e) => setInputValue(e.target.value)}
+                                value={videoUrl}
+                                onChange={(e) => setvideoUrl(e.target.value)}
                             />
                             <button
+                                onClick={handleClaimVideo}
                                 className="rounded-md bg-blue-500 px-4 py-2 text-white shadow-md hover:bg-blue-600"
-                                onClick={handleCheckSubcollection}
                             >
-                                Verificar
+                                Reinvidicar
                             </button>
                         </div>
                     </div>

2f2c1772b120e93cfb5a452aca7641c1c43a37cf
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-06-28 01:23:24 -0300

    style: shadcn ui to claimed videos page

--- app/claimedvideos/page.tsx
+++ app/claimedvideos/page.tsx
@@ -8,17 +8,32 @@ import {
     setDoc,
     getDoc,
 } from 'firebase/firestore'
-import { increment } from 'firebase/firestore'
 import Header from '../../components/Header/Header'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import { getAuth } from 'firebase/auth'
 import firebaseConfig from '../firebase-config'
 import { initializeApp } from 'firebase/app'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
 
 interface UserVideo {
     video_url: string
     position: number
+    title: string
+    thumbnailUrl: string
+}
+
+interface VideoData {
+    id: string
+    title: string
+    thumbnailUrl: string
 }
 
 export default function ClaimedVideos() {
@@ -33,6 +48,9 @@ export default function ClaimedVideos() {
     const [loggedUser, setLoggedUser] = useState<unknown | null>(null)
     const [user_id, setUser_id] = useState<string | null>(null) // Estado para armazenar user_id
     const [userVideos, setUserVideos] = useState<UserVideo[]>([])
+    const [videoUrl, setVideoUrl] = useState<string>('')
+    const [videoData, setVideoData] = useState<VideoData | null>(null)
+    const [error, setError] = useState<string | null>(null)
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -66,7 +84,7 @@ export default function ClaimedVideos() {
                     throw usersError
                 }
 
-                //setando o id do usuario
+                // Setando o id do usuário
                 const user_id = users ? users.id : null
                 setUser_id(user_id)
 
@@ -78,7 +96,45 @@ export default function ClaimedVideos() {
                 if (error) {
                     throw error
                 }
-                setUserVideos(userVideosData || [])
+
+                // Mapeando os dados dos vídeos para buscar título e thumbnail
+                const videosWithDetails: UserVideo[] = []
+                for (const video of userVideosData || []) {
+                    const videoUrl = video.video_url
+
+                    // Extrair o ID do vídeo do YouTube usando regex ou outra lógica
+                    const videoIdRegex =
+                        /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+                    const match = videoUrl.match(videoIdRegex)
+
+                    if (match && match[1]) {
+                        const videoId = match[1]
+                        const apiKey = 'AIzaSyBFQKXiPBiluxE3jtWnTvZH3A9K76A8afc'
+
+                        // Fazer requisição para a API do YouTube para obter detalhes do vídeo
+                        const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`
+                        const response = await fetch(apiUrl)
+                        const data = await response.json()
+
+                        if (data.items && data.items.length > 0) {
+                            const snippet = data.items[0].snippet
+                            const title = snippet.title
+                            const thumbnailUrl =
+                                snippet.thumbnails.maxres?.url ||
+                                snippet.thumbnails.high.url ||
+                                snippet.thumbnails.medium.url
+
+                            videosWithDetails.push({
+                                video_url: videoUrl,
+                                position: video.position,
+                                title: title,
+                                thumbnailUrl: thumbnailUrl,
+                            })
+                        }
+                    }
+                }
+
+                setUserVideos(videosWithDetails)
             } catch (error) {
                 console.error('Erro ao reinvidicar vídeo:', error)
             }
@@ -91,6 +147,51 @@ export default function ClaimedVideos() {
         }
     }, [authStateChangedComplete])
 
+    // pegar dados dos videos
+    useEffect(() => {
+        if (!videoUrl) return
+
+        // Regex para extrair o ID do vídeo do YouTube
+        const videoIdRegex =
+            /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+
+        const match = videoUrl.match(videoIdRegex)
+        if (match && match[1]) {
+            const videoId = match[1]
+            const apiKey = 'AIzaSyBFQKXiPBiluxE3jtWnTvZH3A9K76A8afc' // Substitua pela sua chave de API do Google
+
+            // Fazer requisição para a API do YouTube para obter detalhes do vídeo
+            const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`
+            fetch(apiUrl)
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(
+                            'Erro na requisição para a API do YouTube'
+                        )
+                    }
+                    return response.json()
+                })
+                .then((data) => {
+                    if (data.items && data.items.length > 0) {
+                        const snippet = data.items[0].snippet
+                        setVideoData({
+                            id: videoId,
+                            title: snippet.title,
+                            thumbnailUrl: snippet.thumbnails.medium,
+                        })
+                    } else {
+                        throw new Error(
+                            'Nenhum item encontrado na resposta da API do YouTube'
+                        )
+                    }
+                })
+                .catch((error) => {
+                    console.error('Erro ao buscar dados do vídeo:', error)
+                    setError('Erro ao buscar dados do vídeo')
+                })
+        }
+    }, [videoUrl])
+
     return (
         <main className="flex min-h-screen flex-col">
             <Header />
@@ -99,19 +200,44 @@ export default function ClaimedVideos() {
                     <Sidebar />
                     <div className="flex flex-col p-4">
                         <p className="mb-4 text-lg font-semibold">
-                            videos reinvidicados pelo usuario:{' '}
+                            Vídeos reivindicados pelo usuário:{' '}
                             {auth.currentUser?.displayName}
                         </p>
                         <div className="grid grid-cols-2 gap-4">
-                            {userVideos.map((video, index) => (
+                            {/* {userVideos.map((video, index) => (
                                 <div key={index} className="border p-4">
                                     <p className="mb-2 text-lg font-semibold">
-                                        {video.video_url}
+                                        {video.title}
                                     </p>
+                                    <img
+                                        src={video.thumbnailUrl}
+                                        alt="Thumbnail do vídeo"
+                                        className="mb-2"
+                                        style={{ maxWidth: '100%' }}
+                                    />
                                     <p className="text-sm">
                                         Position: {video.position}
                                     </p>
                                 </div>
+                            ))} */}
+                            {userVideos.map((video, index) => (
+                                <Card className="max-w-md">
+                                    <CardHeader>
+                                        <img
+                                            src={video.thumbnailUrl}
+                                            alt="Thumbnail do vídeo"
+                                            className="mb-2"
+                                            style={{ maxWidth: '100%' }}
+                                        />
+                                        <CardTitle>{video.title}</CardTitle>
+                                        <CardDescription>
+                                            Card Description
+                                        </CardDescription>
+                                    </CardHeader>
+                                    <CardFooter>
+                                        <p>Position: {video.position}</p>
+                                    </CardFooter>
+                                </Card>
                             ))}
                         </div>
                         <div className="flex flex-col space-y-2"></div>

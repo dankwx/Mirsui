103f22d29fff4b1a117f4c6c57b4b697ef7543ac
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-07-01 20:27:24 -0300

    feature: add svg and some style do delete current claimed video

--- app/user/[slug]/claimed/page.tsx
+++ app/user/[slug]/claimed/page.tsx
@@ -198,14 +198,41 @@ export default function NewClaimedVideos({
                                     >
                                         <CardHeader>
                                             <div
-                                                className="absolute right-0 top-0 z-10 bg-red-700 p-2 opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+                                                className="absolute right-5 top-5 z-10 bg-white p-2 opacity-0 transition-all duration-300 hover:cursor-pointer hover:bg-red-400 hover:text-white group-hover:opacity-100"
                                                 onClick={() => {
                                                     handleDeleteVideo(
                                                         video.video_url
                                                     )
                                                 }}
                                             >
-                                                delete
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="18"
+                                                    height="18"
+                                                    viewBox="0 0 24 24"
+                                                    fill="none"
+                                                    stroke="currentColor"
+                                                    stroke-width="2"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    className="lucide lucide-trash-2"
+                                                >
+                                                    <path d="M3 6h18" />
+                                                    <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                                                    <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                                                    <line
+                                                        x1="10"
+                                                        x2="10"
+                                                        y1="11"
+                                                        y2="17"
+                                                    />
+                                                    <line
+                                                        x1="14"
+                                                        x2="14"
+                                                        y1="11"
+                                                        y2="17"
+                                                    />
+                                                </svg>
                                             </div>
                                             <Link
                                                 href={video.video_url}

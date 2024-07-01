4f369884000efedf2d11361d23a5793884e50292
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-07-01 18:47:30 -0300

    feature: change route newclaimed videos to user/[slug]/claimed

--- app/user/[slug]/claimed/page.tsx
+++ app/user/[slug]/claimed/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import Header from '../../../components/Header/Header'
-import Sidebar from '../../../components/Sidebar/Sidebar'
+import Header from '../../../../components/Header/Header'
+import Sidebar from '../../../../components/Sidebar/Sidebar'
 import { getAuth } from 'firebase/auth'
-import firebaseConfig from '../../firebase-config'
+import firebaseConfig from '../../../firebase-config'
 import { initializeApp } from 'firebase/app'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import {

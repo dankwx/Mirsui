7a0c6b0b16c1526f321f6734557c59388595db81
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-07-03 21:24:42 -0300

    feature: add register modal component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,32 @@
+"use client"
+
 import { login, signup } from './actions'
+import LoginModal from '@/components/ModalLogin/page'
 
 export default function LoginPage() {
-  return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      
-      <label htmlFor="username">Username:</label>
-      <input id="username" name="username" type="text" required />
-      
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
-  )
+    const handleLogin = (email: string, password: string) => {
+        // Lógica de login aqui
+        console.log('Login attempt:', email, password)
+    }
+    return (
+        <div>
+            <LoginModal
+                trigger={<button>Abrir Login</button>}
+                onLogin={handleLogin}
+            />
+            <form>
+                <label htmlFor="email">Email:</label>
+                <input id="email" name="email" type="email" required />
+
+                <label htmlFor="password">Password:</label>
+                <input id="password" name="password" type="password" required />
+
+                <label htmlFor="username">Username:</label>
+                <input id="username" name="username" type="text" required />
+
+                <button formAction={login}>Log in</button>
+                <button formAction={signup}>Sign up</button>
+            </form>
+        </div>
+    )
 }
---
+++ components/ModalLogin/page.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { login, signup } from '../../app/login/actions'
+
+interface LoginModalProps {
+    trigger: React.ReactNode
+    onLogin: (email: string, password: string) => void
+}
+
+const LoginModal: React.FC<LoginModalProps> = ({ trigger, onLogin }) => {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const [username, setUsername] = React.useState('')
+
+
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>{trigger}</DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Log In</DialogTitle>
+                    <DialogDescription>
+                        By continuing, you agree to our User Agreement and
+                        acknowledge that you understand the Privacy Policy.
+                    </DialogDescription>
+                </DialogHeader>
+                <form>
+                    <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="email" className="text-right">
+                                Email
+                            </Label>
+                            <Input
+                                id="email"
+                                name="email"
+                                type="email"
+                                required
+                                className="col-span-3"
+                                value={email}
+                                placeholder="email@email.com"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="password" className="text-right">
+                                Password
+                            </Label>
+                            <Input
+                                id="password"
+                                name="password"
+                                className="col-span-3"
+                                required
+                                type="password"
+                                placeholder="*******"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="username" className="text-right">
+                                Username
+                            </Label>
+                            <Input
+                                id="username"
+                                name="username"
+                                className="col-span-3"
+                                required
+                                type="text"
+                                placeholder="Username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <DialogDescription className="pt-4 text-blue-600">
+                        Forgot Password?
+                    </DialogDescription>
+                    <DialogDescription className="pt-4 text-blue-600">
+                        Don't have an account? Register
+                    </DialogDescription>
+                    <DialogFooter>
+                        <Button type="submit" formAction={signup}>
+                            Register
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default LoginModal

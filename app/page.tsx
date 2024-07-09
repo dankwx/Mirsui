c06788ad624bf4e36c8382757600a30bb5c588d2
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-07-09 13:02:42 -0300

    feature: add claim spotify tracks component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header/Header'
 import Sidebar from '../components/Sidebar/Sidebar'
 import GetLatestClaims from '@/components/GetLatestClaims/GetLatestClaims'
+import ClaimTrack from '@/components/ClaimTrack/ClaimTrack'
 
 export default async function Home() {
     return (
@@ -10,7 +11,8 @@ export default async function Home() {
                 <div className="flex h-full flex-1">
                     <Sidebar />
                     <div className="flex flex-col">
-                        <GetLatestClaims />
+                        {/* <GetLatestClaims /> */}
+                        <ClaimTrack />
                     </div>
                 </div>
             </div>

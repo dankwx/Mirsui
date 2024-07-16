dae898598350df738373e2885c948b1d87f5e025
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-07-16 15:34:45 -0300

    chore: add icon to expanded sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import Link from 'next/link'
 import { useState } from 'react'
@@ -7,20 +7,20 @@ export default function Sidebar() {
     const [isExpanded, setIsExpanded] = useState(false)
 
     return (
-        <aside 
+        <aside
             className={`flex min-h-11 border-r-2 border-solid border-gray-200 bg-gray-50 pt-16 transition-all duration-300 ease-in-out ${isExpanded ? 'min-w-64' : 'min-w-16'}`}
             onMouseEnter={() => setIsExpanded(true)}
             onMouseLeave={() => setIsExpanded(false)}
         >
-            <div className="fixed flex w-fit flex-col text-black overflow-hidden whitespace-nowrap">
+            <div className="fixed flex w-fit flex-col overflow-hidden whitespace-nowrap text-black">
                 <Link href="/getchannel" className="p-2 hover:bg-gray-200">
-                    {isExpanded ? 'Claim Channel' : '📺'}
+                    {isExpanded ? '📺 Claim Channel' : '📺'}
                 </Link>
                 <Link href="/claimvideo" className="p-2 hover:bg-gray-200">
-                    {isExpanded ? 'Claim Video' : '🎥'}
+                    {isExpanded ? '🎥 Claim Video' : '🎥'}
                 </Link>
                 <Link href="/claimtrack" className="p-2 hover:bg-gray-200">
-                    {isExpanded ? 'Claim Track' : '🎵'}
+                    {isExpanded ? '🎵 Claim Track' : '🎵'}
                 </Link>
             </div>
         </aside>

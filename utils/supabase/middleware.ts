6102d181a821fbd8b8a760b0514b3e5db4909f44
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-07-18 20:38:48 -0300

    chore: add user path to be acessible without login

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -39,7 +39,8 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
-    request.nextUrl.pathname !== '/' // Adicione esta condição
+    request.nextUrl.pathname !== '/' &&
+    !request.nextUrl.pathname.startsWith('/user')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()

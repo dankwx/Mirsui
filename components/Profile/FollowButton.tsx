af64aca2e7a3cc4533d92041210caa2a36caec33
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-11-20 23:09:10 -0300

    feat: add followButton variants

--- components/Profile/FollowButton.tsx
+++ components/Profile/FollowButton.tsx
@@ -5,12 +5,16 @@ import { Button } from '@/components/ui/button'
 import { toggleFollow } from './actions'
 import { UserPlus, UserMinus, Loader2 } from 'lucide-react'
 
+type ButtonType = 'icon' | 'text'
+
 interface FollowButtonProps {
   followingId: string
   initialIsFollowing: boolean
+  type?: ButtonType
+  variant?: 'default' | 'secondary' | 'outline'
 }
 
-export default function FollowButton({ followingId, initialIsFollowing }: FollowButtonProps) {
+export default function FollowButton({ followingId, initialIsFollowing,type='icon', variant='default' }: FollowButtonProps) {
   const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
   const [isLoading, setIsLoading] = useState(false)
 
@@ -28,18 +32,27 @@ export default function FollowButton({ followingId, initialIsFollowing }: Follow
     }
   }
 
+  if (type === 'icon') {
+    return (
+      <Button 
+        variant={variant}
+        className='relative w-8 h-8 p-0'
+        onClick={handleToggleFollow} 
+        disabled={isLoading}
+        title={isFollowing ? 'Unfollow' : 'Follow'}
+      >
+        {isLoading ? (
+          <Loader2 className="h-5 w-5 animate-spin" />
+        ) : (
+          isFollowing ? <UserMinus className="h-5 w-5" /> : <UserPlus className="h-5 w-5" />
+        )}
+      </Button>
+    )
+  }
+
   return (
-    <Button 
-      className='relative w-8 h-8 p-0'
-      onClick={handleToggleFollow} 
-      disabled={isLoading}
-      title={isFollowing ? 'Unfollow' : 'Follow'}
-    >
-      {isLoading ? (
-        <Loader2 className="h-5 w-5 animate-spin" />
-      ) : (
-        isFollowing ? <UserMinus className="h-5 w-5" /> : <UserPlus className="h-5 w-5" />
-      )}
-    </Button>
+    <Button variant="outline" size="sm">
+                                    Unfollow                                                            {/* BOTAO DE UNFOLLOW*/}
+                                </Button>
   )
 }

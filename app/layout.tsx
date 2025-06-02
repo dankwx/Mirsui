e5ffedf306957bc9212a99bfeb80a3aa78283ede
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2025-06-02 19:03:59 -0300

    fix: fix website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from '@/components/ui/toaster'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-    title: 'SoungSage',
+    title: 'SoundSage',
     description:
         'Claim your music and prove that you litened to it before it got popular',
 }

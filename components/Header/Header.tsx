deed2b78cf0bc7bc290ce40d324f79d8f538bad3
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2025-06-03 19:13:27 -0300

    feat: new working sidebar wit proper visual

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,11 +4,10 @@ import SearchWithResults from '../SearchWithResults/SearchWithResults'
 
 export default function Header() {
     return (
-        <main className="fixed sticky top-0 z-10 z-50 flex h-16 w-full flex-col border-b bg-white bg-white/80 backdrop-blur-sm">
-            <div className="flex flex-grow items-center justify-center border-b-2 border-solid border-gray-200 font-sans font-medium">
+        <header className="flex h-16 w-full shrink-0 items-center border-b-2 border-solid border-gray-200 bg-white font-sans font-medium">
+            <div className="flex w-full items-center justify-center">
                 <div className="flex w-3/4 items-center">
                     <div className="flex w-full items-center justify-between text-xl">
-                        <Logo />
                         <SearchWithResults />
                         <div className="flex items-center justify-center">
                             <GetAuth />
@@ -16,6 +15,6 @@ export default function Header() {
                     </div>
                 </div>
             </div>
-        </main>
+        </header>
     )
 }

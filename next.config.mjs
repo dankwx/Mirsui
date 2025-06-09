92629a393ffb561a17a6dbfc81961cccac0950b0
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2025-06-09 00:06:10 -0300

    chore: update packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
     images: {
         domains: ['i.scdn.co', 'tqprioqqitimssshcrcr.supabase.co'],
     },
